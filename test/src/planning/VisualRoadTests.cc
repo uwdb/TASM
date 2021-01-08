f45ab67944d74907d7e531d3c7e551bfc6155ea0
Maureen Daum <mdaum@cs.washington.edu>
2021-01-08 08:24:50 -0500

    Create black tiles via LightDB

--- test/src/planning/VisualRoadTests.cc
+++ test/src/planning/VisualRoadTests.cc
@@ -38,6 +38,40 @@ void DeleteDatabase(std::string id) {
     remove(dbPath.c_str());
 }
 
+TEST_F(VisualRoadTestFixture, testReencodeBlackTile) {
+    std::string blackTilePath("/home/maureen/black_tiles/30_f/320_w/256_h/t_320_256.hevc");
+    auto input = Load(blackTilePath, Volume::limits(), GeometryReference::make<EquirectangularGeometry>(EquirectangularGeometry::Samples()));
+    Coordinator().execute(input.Encode(Codec::hevc(), {{EncodeOptions::GOPSize, 30u}}).Save("/home/maureen/masked_videos/t_320_256_reencoded.hevc"));
+}
+
+TEST_F(VisualRoadTestFixture, testCreateBlackTiles) {
+    unsigned int minWidth = 256;
+    unsigned int maxWidth = 3840;
+    unsigned int minHeight = 160;
+    unsigned int maxHeight = 1984;
+    unsigned int alignment = 32;
+    unsigned int numFrames = 30;
+    std::filesystem::path base("/home/maureen/lightdb_created_black_tiles");
+    Codec codec = Codec::hevc();
+
+    for (auto w = minWidth; w <= maxWidth; w += alignment) {
+        for (auto h = minHeight; h <= maxHeight; h += alignment) {
+            std::cout << "Creating " << w << " x " << h << " black tile" << std::endl;
+            auto outputPath = catalog::BlackTileFiles::pathForTile(base, numFrames, w, h);
+            std::filesystem::create_directories(outputPath.parent_path());
+            Coordinator().execute(CreateBlackTile(codec, w, h, numFrames).Encode(codec, {{EncodeOptions::GOPSize, numFrames}}).Save(outputPath));
+        }
+    }
+}
+
+TEST_F(VisualRoadTestFixture, testSetUpVideoForTiling) {
+    std::string videoPath("/home/maureen/masked_videos/cropped_traffic-4k-002-ds2k.mp4");
+    std::string storedName("traffic-4k-002-ds2k-cropped-cracked");
+    unsigned int framerate = 30;
+    auto input = Load(videoPath, Volume::limits(), GeometryReference::make<EquirectangularGeometry>(EquirectangularGeometry::Samples()));
+    Coordinator().execute(input.PrepareForCracking(storedName, framerate));
+}
+
 TEST_F(VisualRoadTestFixture, testDetectRunYolo) {
     std::string videoId("traffic-4k-002-ds2k-not-tiled-cracked");
     DeleteDatabase(videoId);
@@ -126,8 +160,8 @@ TEST_F(VisualRoadTestFixture, testDetectAndMaskAroundPeople) {
     auto yolo = lightdb::extensibility::Load("yologpu");
 
 //    DeleteDatabase(videoId);
-//    DeleteTiles(videoId);
-//    ResetTileNum(videoId);
+    DeleteTiles(videoId);
+    ResetTileNum(videoId);
 
     // First, detect objects.
 //    auto input = ScanMultiTiled(videoId);
@@ -135,15 +169,15 @@ TEST_F(VisualRoadTestFixture, testDetectAndMaskAroundPeople) {
 //    Coordinator().execute(input.Select(selection).Map(yolo));
 
     // Then, re-tile around people.
-    PixelsInFrameMetadataSpecification personSelection(std::make_shared<SingleMetadataElement>("label", "car", 30, 60));
-//    auto framerate = 30u;
-//    auto retileOp = ScanAndRetile(
-//            videoId,
-//            "traffic-4k-002-ds2k",
-//            personSelection,
-//            framerate,
-//            CrackingStrategy::GroupingExtent);
-//    Coordinator().execute(retileOp);
+    PixelsInFrameMetadataSpecification personSelection(std::make_shared<SingleMetadataElement>("label", "person", 30, 60));
+    auto framerate = 30u;
+    auto retileOp = ScanAndRetile(
+            videoId,
+            "traffic-4k-002-ds2k",
+            personSelection,
+            framerate,
+            CrackingStrategy::GroupingExtent);
+    Coordinator().execute(retileOp);
 
     // Then, do selection on people tiles.
     // Re-do scan so that new tiles are discovered.

e398da998fc6ee121e063ae60850e2aa6f5e0132
maureendaum <mdaum@cs.washington.edu>
2021-02-02 11:34:04 -0800

    Work on example notebook
    - The Dockerfile will have to be cleaned up with some steps moved to the
    environment file, but I'll do that once all of the packages are finalized.
    - Install the python library so we can use it from other locations.
    - Start a Jupyter notebook under python/Examples that walks through the usage.
    - Add a sample video/detections to start with.

---
+++ python/python_package/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="tasm",
+    version="0.0.1",
+    author="Maureen Daum",
+    author_email="mdaum@cs.washington.edu",
+    description="TASM video storage manager",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/maureendaum/TASM",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
